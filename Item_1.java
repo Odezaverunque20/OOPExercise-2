450aefa6f86536b7f788b17c70137d89ffcd14e8
Odeza M. Verunque <116275902+Odezaverunque20@users.noreply.github.com>
2022-10-31 23:09:42 +0800

    Update Item_1.java

--- Item_1.java
+++ Item_1.java
@@ -1,3 +1,20 @@
-public class Item_1
+public class Item_1 extends Shopee
 {
+  String Item_info;
+	public Item_1(String product_name, int price, String Item_info) {
+		super(product_name, price);
+		this.Item_info=Item_info;
+	}
+
+	@Override
+	public String item_info() {
+		return "This is your First Item Information.";
+	}
+
+	public String getItemInfo() {
+		return Item_info;
+	}
+	public void setItemInfo(String Item_info) {
+		this.Item_info=Item_info;
+	}
 }
